898cf885dcf147533b070a84947b0dd42f3d9d11
Yoominseo <min123seo@naver.com>
2025-01-27 17:34:17 +0900

    나눔 스퀘어 폰트 추가

---
+++ app/(route)/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "@/_styles/globals.css";
+
+export const metadata: Metadata = {
+  title: "팀 게임 멤버 추첨",
+  description: "여러가지 팀 게임 멤버를 추첨해 보세요!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`antialiased`}>{children}</body>
+    </html>
+  );
+}
